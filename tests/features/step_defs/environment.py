20ba5f8968c5ceeb7105b3c828f47df4a0180e8d
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-16 14:38:55 +0530

    feature file added

---
+++ tests/features/step_defs/environment.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+from Utilities.utils_config_reader import configuration_reader
+import allure
+
+
+# This is behave hook(before_scenario,after_scenario, before_step, after_step, before_all, after_all)
+
+def before_scenario(context, driver):
+    if configuration_reader("basic configure", "browser") == "chrome":
+        context.driver = webdriver.Chrome(ChromeDriverManager().install())
+        context.driver.maximize_window()
+        context.driver.implicitly_wait(10)
+    elif configuration_reader("basic configure", "browser") == "firefox":
+        context.driver = webdriver.Firefox(GeckoDriverManager().install())
+        context.driver.maximize_window()
+        context.driver.implicitly_wait(10)
+
+
+def after_scenario(context, driver):
+    context.driver.quit()
+
+
+def after_step(context, step):
+    if step.status == 'failed':
+        allure.attach(context.driver.get_screenshot_as_png(), name='screenshot',
+                      attachment_type=allure.attachment_type.PNG)
