4e67bee4c2670f8e9277b346320227c5f91c2f0c
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-17 18:40:12 +0530

    step definition file added for login.

--- tests/features/step_defs/test_login_demo_webshop.py
+++ tests/features/step_defs/test_login_demo_webshop.py
@@ -1,7 +1,7 @@
 """Test Demo Web Shop feature tests."""
 # To generate code snippet use "pytest-bdd generate .\tests\features\demo_webshop.feature" code
 
-from pytest_bdd import *
+from pytest_bdd import scenario, given, when, then, parsers
 
 
 @scenario('features\demo_webshop.feature', 'Test Login functionality of Demo webshop')
@@ -10,25 +10,25 @@ def test_login_functionality_of_demo_webshop():
 
 
 @given('user opens the Demo Web Shop url')
-def user_opens_the_demo_web_shop_url():
+def open_demo_web_shop_url(get_browser):
     pass
 
 
 @when('click on the LOG IN button')
-def click_on_the_log_in_button():
+def click_log_in_button(get_browser):
     pass
 
 
-@when('user enter the <email> and <password>')
-def user_enter_the_email_and_password():
+@when(parsers.parse('user enter the <email> and <password>'))
+def enter_email_password(get_browser):
     pass
 
 
 @then('user is able to successfully landed on the Home page')
-def user_is_able_to_successfully_landed_on_the_home_page():
+def landed_on_the_home_page(get_browser):
     pass
 
 
 @then('verify the page title is matching <home_page_title>')
-def verify_the_page_title_is_matching_home_page_title():
+def verify_home_page_title(get_browser):
     pass
