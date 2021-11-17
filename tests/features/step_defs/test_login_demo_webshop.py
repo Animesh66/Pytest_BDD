bb3381d00be08d9be065c943621a8a90a9dcc40b
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-11-17 12:35:12 +0530

    step definition file added for login.

--- tests/features/step_defs/test_login_demo_webshop.py
+++ tests/features/step_defs/test_login_demo_webshop.py
@@ -4,35 +4,29 @@ from pytest_bdd import *
 
 
 @scenario('features\demo_webshop.feature', 'Test Login functionality of Demo webshop')
-def test_test_login_functionality_of_demo_webshop():
-    """Test Login functionality of Demo webshop."""
-
+def test_login_functionality_of_demo_webshop():
+    pass
 
 @given('user opens the Demo Web Shop url')
 def user_opens_the_demo_web_shop_url():
-    """user opens the Demo Web Shop url."""
-    raise NotImplementedError
+    pass
 
 
 @when('click on the LOG IN button')
 def click_on_the_log_in_button():
-    """click on the LOG IN button."""
-    raise NotImplementedError
+    pass
 
 
 @when('user enter the <email> and <password>')
 def user_enter_the_email_and_password():
-    """user enter the <email> and <password>."""
-    raise NotImplementedError
+    pass
 
 
 @then('user is able to successfully landed on the Home page')
 def user_is_able_to_successfully_landed_on_the_home_page():
-    """user is able to successfully landed on the Home page."""
-    raise NotImplementedError
+    pass
 
 
 @then('verify the page title is matching <home_page_title>')
 def verify_the_page_title_is_matching_home_page_title():
-    """verify the page title is matching <home_page_title>."""
-    raise NotImplementedError
+    pass
