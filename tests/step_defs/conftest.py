50bb2597cfa3b3b938b1fd6fd48bff17a39c0dc1
Animesh_Mukherjee <er.animesh6@hotmail.com>
2021-12-10 12:08:06 +0530

    Allure report generated successfully.

--- tests/step_defs/conftest.py
+++ tests/step_defs/conftest.py
@@ -4,10 +4,22 @@ import pytest
 from webdriver_manager.firefox import GeckoDriverManager
 
 
+# @pytest.fixture(params=["chrome", "firefox"])
+# def get_browser(request):
+#     if request.param == "chrome":
+#         driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+#     if request.param == "firefox":
+#         driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
+#     request.driver = driver
+#     driver.maximize_window()
+#     driver.implicitly_wait(10)
+#     yield driver
+#     driver.quit()
+
 @pytest.fixture()
 def get_browser():
     driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
-    driver.implicitly_wait(10)
     driver.maximize_window()
+    driver.implicitly_wait(10)
     yield driver
     driver.quit()
